85b74652fcd626966d2f4fb5f4a8a2a119f1b43b
sahilkhadtare <88779109+sahilkhadtare@users.noreply.github.com>
2021-12-15 11:59:20 +0530

    Add files via upload

--- oeAssignment4Sahil/js/view-models/sample.js
+++ oeAssignment4Sahil/js/view-models/sample.js
@@ -30,33 +30,24 @@ export class Sample extends BaseWidget {
 
     @exportToViewModel dataArray = ko.observableArray([]);
     @exportToViewModel gotoCollectionpage(path) {
-        window.open(path);
+        window.open(path, '_blank');
     }
     beforeAppear() {
         var array = ["Sahil_Centennial", "Sahil_Kanati", "Sahil_GBC", "Sahil_Greenball"]
         let data = {};
         data[ccConstants.CATEGORY_IDS] = array.join();
+        let widget = this.$data;
         ccRestClient.request(
             ccConstants.ENDPOINT_LIST_COLLECTIONS,
             data,
             (response) => {
                 response.map(data => {
-                    console.log("sahil", data);
-                    this.dataArray.push(new dataObject(data.displayName, `/file${data.categoryImages[0].path}`, data.route))
-
+                    widget.dataArray.push(new dataObject(data.displayName, `/file${data.categoryImages[0].path}`, data.route))
                 });
             },
             (error) => { console.log("Im error", error) }
         );
         console.log('[BEFORE APPEAR] Sample');
-        var images = document.getElementsByClassName("brand_card");
-        for (let i = 0; i < images.length; i++) {
-            var image = images[i];
-            image.onclick = function(event) {
-                window.open(dataArray.route);
-            };
-        }
-
     }
 
 
