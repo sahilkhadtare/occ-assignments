13406dc6b973625dcf71c98a4357193e768667ab
sahilkhadtare <88779109+sahilkhadtare@users.noreply.github.com>
2021-12-16 17:23:27 +0530

    Delete oeAssignment9Sahilsse directory

--- oeAssignment9Sahilsse/jest.config.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-  name: 'oeAssignment9Sahil',
-  verbose: true,
-  testEnvironment: 'node',
-  testMatch: ['**/__tests__/**/*.(spec|test).js?(x)'],
-  collectCoverage: true,
-  collectCoverageFrom: ['app/**/*.{js,jsx}', '!**/node_modules/**', '!app/__tests__/testUtils/**'],
-  coverageReporters: ['json', 'text']
-};
